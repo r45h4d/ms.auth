1d6139ac51ec86734108382b49be29434468e285
rashadamanovv <rashadamanovv@gmail.com>
2024-10-08 11:42:18 +0400

    Prepare commit

--- src/main/java/az/ingress/service/abstraction/UserService.java
+++
@@ -1,9 +0,0 @@
-package az.ingress.service.abstraction;
-
-import az.ingress.model.dto.UserResponseDto;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UserService {
-    UserResponseDto getUserDetails(String username);
-}
